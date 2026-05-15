353afcef70a328c8e797c91796ab2357d48fa4ea
muhamadghoul854-gif <muhamadghoul854@gmail.com>
2026-05-15 07:09:02 +0300

    Update next.config.js

--- souq-pilot/next.config.js
+++ souq-pilot/next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // هيدا السطر بيخلي فيرسيل يتجاهل أخطاء التايب سكريبت ويكمّل بناء
     ignoreBuildErrors: true,
   },
   eslint: {
-    // هيدا السطر بيخليه يتجاهل أخطاء التنسيق (Linting)
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
